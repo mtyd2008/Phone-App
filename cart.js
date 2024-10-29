c56dd2ffb39aca0567275227e8454c20241b459f
mtyd2008 <tahaydandya@gmail.com>
2024-10-30 04:03:01 +0500

    half

--- cart.js
+++ cart.js
@@ -7,7 +7,7 @@ console.log(convert);
 
 const div = document.querySelector("div")
 
-convert.map((item, )=>{
+convert.map((item)=>{
     div.innerHTML += `<div class="card-body">
         <li>Brand: ${item.brand}</li>
         <li>Model: ${item.model}</li>
@@ -15,18 +15,30 @@ convert.map((item, )=>{
         <li>Rom: ${item.rom}</li>
         <li>Camera: ${item.camera}</li>
         <li>Price: ${item.price}</li>
-        <button class="btn btn-primary">+</button>
-        <button class="btn btn-primary">-</button>
+        <div class="d-flex gap-2 mt-2">
+        <button class="btn btn-primary" onclick="plus()">+</button>
+        <h1 class="count">0</h1>
+        <button class="btn btn-primary" onclick="minus()">-</button>
+        </div>
         </div>`
 })
 
+let count = 0
+
+function plus(){
+    count += 1
+    h1.innerHTML = count
+}
+
+function minus(){
+    count -= 1
+    h1.innerHTML = count
+}
 
 
-
-
-
-
-
+if(count === -1){
+    
+}
 
 
 
